ec3695a666445f393c3bf5d13270aae5d9e6fbf8
yussan <yusuf@sebangsa.com>
2019-10-23 13:08:46 +0700

    initial project

--- 1.0. getting started/src/components/index.jsx
+++ 1.0. getting started/src/components/index.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { connect } from "react-redux"
 
 class IndexComponent extends React.Component {
+
   render() {
     return (
       <div>
--- 1.0. getting started/src/redux/reducers/index.js
+++ 1.0. getting started/src/redux/reducers/index.js
@@ -3,8 +3,7 @@ import { FETCH_NEWS, fetchNews } from "../actions/index"
 export default (state = {}, action) => {
   switch(action.type) {
     case FETCH_NEWS:
-      return state  
-    // return {...state, {loading: true}}
+      return {...state, loading: true }
     default: 
       return state
   }
